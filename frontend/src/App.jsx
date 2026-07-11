32d3ea7c2432e8b84774227d985c0de61dccd574
aditya-nambiar2007 <nambiaraditya2007@gmail.com>
2026-07-11 15:03:10 +0530

    Added history Loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,11 +23,18 @@ function App() {
     setHistoryVersion(v => v + 1)
   }
 
-  const load=async()=>{
-    setHead(await history.getNode('43749820-ad1b-4c27-aa9c-579f86508f56'))
-    setImageHistoryNode(await history.getNode('43749820-ad1b-4c27-aa9c-579f86508f56'))
+  const load=async(uploadId)=>{
+    try{
+      if(!uploadId)return;
+      setHead(await history.getNode(uploadId))
+      setImageHistoryNode(await history.getNode(uploadId))
+    }
+    catch (err){
+      console.log(err);
+      return;
+    }
   }
-  load();
+  load('43749820-ad1b-4c27-aa9c-579f86508f56');
   
   const setNode = async (nodeId) => {
     if (!nodeId) return
